518c9da6472f7f9f9ccc5de45f26794bb0b75bfd
buribalazs@gmail.com <buribalazs@gmail.com>
2017-04-14 19:39:50 +0200

    proxyservice stub

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-let proxyService = require('./js/proxy-service');
+const request = require('request');
+const proxyService = require('./js/proxy-service');
+
+proxyService.getProxy().then(proxy => {
+    console.log(proxy);
+    request({
+        url: 'http://' + proxy.ip + ':' + proxy.port,
+        timeout: 5000,
+    }, (err, res, html) => {
+
+        console.log(err, res, html);
+    });
+}).catch(m => console.log(m));
--- js/proxy-service.js
+++ js/proxy-service.js
@@ -1,8 +1,40 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+const UPDATE_FREQ = 3600000;
+
+let lastUpdate = undefined;
 let proxies = [];
 
-let promise = new Promise((resolve, reject) => {
+function getProxy() {
+    return new Promise((resolve, reject) => {
+        if (!lastUpdate || new Date().getTime > lastUpdate + UPDATE_FREQ) {
+            request('https://free-proxy-list.net/', (err, res, html) => {
+                if (!err) {
+                    let $ = cheerio.load(html, {decodeEntities: false});
+                    $('#proxylisttable tbody tr').each((index, item) => {
+                        let tds = $(item).find('td');
+                        let result = {
+                            ip: $(tds[0]).text().trim(),
+                            port: $(tds[1]).text().trim(),
+                        };
+                        if (!proxies.some(item => item.ip === result.ip)) {
+                            proxies.push(result);
+                        }
+                    });
+                }
+                findGoodProxy().then(resolve).catch(reject);
+            });
+        }
+    });
+}
 
-});
+function findGoodProxy(){
+    return new Promise((resolve, reject) => {
+
+    });
+}
+
+module.exports = {
+    getProxy,
+};
