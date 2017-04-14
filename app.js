0a0e859d36811e3070f452c824eaf1bf6e50cd4d
buribalazs@gmail.com <buribalazs@gmail.com>
2017-04-14 18:14:29 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,1 @@
+let proxyService = require('./js/proxy-service');
---
+++ js/proxy-service.js
@@ -0,0 +1,8 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+let proxies = [];
+
+let promise = new Promise((resolve, reject) => {
+
+});
