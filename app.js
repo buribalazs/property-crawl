acabcb152466c39a08343290035d34d1a31214eb
buribalazs@gmail.com <buribalazs@gmail.com>
2017-04-14 21:03:13 +0200

    proxyservice get good proxy sweep

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const request = require('request');
 const proxyService = require('./js/proxy-service');
 
-proxyService.getProxy().then(proxy => {
-    console.log(proxy);
-    request({
-        url: 'http://' + proxy.ip + ':' + proxy.port,
-        timeout: 5000,
-    }, (err, res, html) => {
+// proxyService.getProxy().then(console.log(proxy);
+//     request({
+//         url: 'http://' + proxy.ip + ':' + proxy.port,
+//         timeout: 5000,
+//     }, (err, res, html) => {
+//
+//         console.log(err, res, html);
+//     });
+// }).catch(m => console.log(m));
 
-        console.log(err, res, html);
-    });
-}).catch(m => console.log(m));
+proxyService.getProxy().then(console.log);
--- js/proxy-service.js
+++ js/proxy-service.js
@@ -2,21 +2,32 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 const UPDATE_FREQ = 3600000;
+const COOL_TIME = 28800000;
 
 let lastUpdate = undefined;
+let currentProxy = undefined;
 let proxies = [];
 
 function getProxy() {
+
+    if (currentProxy) {
+        currentProxy.spent = Date.now();
+    }
+
     return new Promise((resolve, reject) => {
-        if (!lastUpdate || new Date().getTime > lastUpdate + UPDATE_FREQ) {
+        if (!lastUpdate || Date.now() > lastUpdate + UPDATE_FREQ) {
             request('https://free-proxy-list.net/', (err, res, html) => {
                 if (!err) {
+                    lastUpdate = Date.now();
                     let $ = cheerio.load(html, {decodeEntities: false});
                     $('#proxylisttable tbody tr').each((index, item) => {
                         let tds = $(item).find('td');
                         let result = {
                             ip: $(tds[0]).text().trim(),
                             port: $(tds[1]).text().trim(),
+                            strikes: 0,
+                            lastDead: 0,
+                            spent: 0,
                         };
                         if (!proxies.some(item => item.ip === result.ip)) {
                             proxies.push(result);
@@ -29,9 +40,35 @@ function getProxy() {
     });
 }
 
-function findGoodProxy(){
+function findGoodProxy() {
     return new Promise((resolve, reject) => {
+        function walk() {
+            let proxy = proxies.find(item => item.strikes < 3 && item.lastDead < Date.now() - UPDATE_FREQ && item.spent < Date.now() - COOL_TIME);
+            if (!proxy) {
+                return reject('no proxy available');
+            }
+            request({
+                url: 'http://ingatlan.com/',
+                // url: 'http://ip.jsontest.com/',
+                proxy: 'http://' + proxy.ip + ':' + proxy.port,
+                timeout: 5000,
+            }, (err, res, html) => {
+                if (err ||
+                    (html && /error|unable|unavailable|service|failure|backend|gateway/g.test(html.slice(0, 512).toLowerCase()))) {
+                    proxy.strikes++;
+                    proxy.lastDead = Date.now();
+                    console.log(proxy);
+                    walk();
+                } else {
+                    if (html) {
+                        console.log(html.slice(0, 512));
+                    }
+                    resolve(proxy);
+                }
+            });
+        }
 
+        walk();
     });
 }
 
